3700ac03a51eb83259496c93acef25cfe1b77f01
pujapyd <pujapal8190@gmail.com>
2024-01-05 16:25:40 +0600

    added.html

---
+++ grade.py
@@ -0,0 +1,18 @@
+ #calculate the grade of a students
+grade =int(input("what is the grade percentage:"))
+if grade >= 90:
+ print("you got excellent")
+elif grade >= 80:
+ print("you got A+")
+elif grade >= 70:
+ print("you got A")
+elif grade >= 60:
+ print("you got A-")
+elif grade >= 50:
+ print("you got B")
+elif grade >= 40:
+ print("you got c")
+elif grade >= 30:
+ print("you got f")
+else:
+ print("sorry you need A- for qualify:")
